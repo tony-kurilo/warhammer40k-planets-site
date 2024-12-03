5295fcd0d8834bc9792b2d13010e3910f4991d10
tony-kurilo <lang.nattreise@gmail.com>
2024-12-03 22:29:23 +0200

    configured db + wrote api that fetches planets names for search page

---
+++ src/pages/api/fetchPlanetsNames.js
@@ -0,0 +1,16 @@
+import pool from "../../app/database/db"
+
+// req = HTTP incoming message, res = HTTP server response
+export default async function handler(req, res) {
+    if (req.method === "GET") {
+        const result = await pool.query("SELECT * FROM planets");
+
+        if (result.rows.length === 0) {
+            return res.status(404).send("No planets");
+        }
+
+        res.status(200).send(result.rows);
+    } else {
+        return res.status(405).send("No planets");
+    }
+}
