ef6738b0705ed3e43889136fd4c3af260386ee90
tony-kurilo <lang.nattreise@gmail.com>
2024-12-11 15:51:22 +0200

    added responsive design

--- src/app/planet/[id]/components/Layout.js
+++ src/app/planet/[id]/components/Layout.js
@@ -14,7 +14,7 @@ export default function Layout({planets_data}) {
     return (
         <Suspense fallback={<div className={'text-primary-text-color'}>Loading data...</div>}>
             <div>
-                <div className="container text-primary-text-color">
+                <div className="text-primary-text-color">
                     <p className={"mt-2"}></p>
                     <a href={"/search"} className={'m-2 mt-2 back-to-search'}>/back-to-search</a>
                     {planets_data.map((planet) => {
@@ -22,11 +22,17 @@ export default function Layout({planets_data}) {
                         return (
                             <div key={planet.id} className={''}>
                                 <div className={'planet-info-container'}>
-                                    <div className={'flex space-x-6 space-y-6'}>
+                                    <div className={'planet-info space-x-6 space-y-6'}>
                                         <div>
-                                            <Image src={imageUrl} width={250} height={250} alt={planet.name}/>
+                                            <Image
+                                                src={imageUrl}
+                                                layout="responsive"
+                                                className="responsive-image"
+                                                width={1}
+                                                height={1} // Пример пропорций
+                                                alt={planet.name}/>
                                         </div>
-                                        <div className={"space-y-5"}>
+                                        <div className={"info-row"}>
                                             <div>
                                                 <h2>Planet Name </h2>
                                                 <h1>{planet.name}</h1>
@@ -35,8 +41,6 @@ export default function Layout({planets_data}) {
                                                 <h2>Segmentum </h2>
                                                 <h1>{planet.segmentum}</h1>
                                             </div>
-                                        </div>
-                                        <div className={"space-y-5"}>
                                             <div>
                                                 <h2>Orbital Radius </h2>
                                                 <h1>{planet.orbit}</h1>
@@ -45,8 +49,6 @@ export default function Layout({planets_data}) {
                                                 <h2>Sector </h2>
                                                 <h1>{planet.sector}</h1>
                                             </div>
-                                        </div>
-                                        <div className={"space-y-5"}>
                                             <div>
                                                 <h2>Sub-Sector </h2>
                                                 <h1>{planet.subsector}</h1>
@@ -55,8 +57,6 @@ export default function Layout({planets_data}) {
                                                 <h2>System </h2>
                                                 <h1>{planet.system}</h1>
                                             </div>
-                                        </div>
-                                        <div className={"space-y-5"}>
                                             <div>
                                                 <h2>Affiliation </h2>
                                                 <h1>{planet.affiliation}</h1>
@@ -65,8 +65,6 @@ export default function Layout({planets_data}) {
                                                 <h2>Population </h2>
                                                 <h1>{planet.population}</h1>
                                             </div>
-                                        </div>
-                                        <div className={"space-y-5"}>
                                             <div>
                                                 <h2>Class </h2>
                                                 <h1>{planet.class}</h1>
@@ -86,18 +84,15 @@ export default function Layout({planets_data}) {
                                                 Description
                                         </button>
                                         {Array.isArray(planet.events) && planet.events.length > 0 && (
-                                            <div className="flex ">
+                                            <div className="flex">
                                                 {planet.events.map((event) => (
-                                                    <div key={event.id} className="event-item">
+                                                    <div key={event.id}>
                                                         <div>
                                                             <button
                                                                 className={`event-button ${activeButton === `${planet.id}-event-${event.id}` ? 'active' : ''}`}
                                                                 onClick={() => handleButtonClick(`${event.name}: ${event.event}`, `${planet.id}-event-${event.id}`)}>
                                                                 {event.date}
                                                             </button>
-                                                        </div>
-                                                        <div>
-
                                                         </div>
                                                     </div>
                                                 ))}
